c3860ff6ac95456b3f8856ff7f461f1a9621c27b
Michael <132103574+MichaelObasa@users.noreply.github.com>
2025-12-02 23:58:56 +0000

    Update EchoPayPuller.sol header comments

--- echopay/contracts/EchoPayPuller.sol
+++ echopay/contracts/EchoPayPuller.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.30;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
-
+ 
 /**
  * @title EchoPayPuller
  * @notice Core contract for recurring USDC investments into ERC4626 vaults (e.g. Indexr funds)
